8fdf4486e300ce5fdb5950d312cab8c603057d49
Bram Speeckaert <bram.speeckaert@gmail.com>
2020-02-16 22:56:03 +0100

    Update to latest libco (v20)

--- amd64.c
+++ amd64.c
@@ -79,26 +79,6 @@ static void (*co_swap)(cothread_t, cothread_t) = 0;
   }
 #else
   /* ABI: SystemV */
-  #ifdef LIBCO_INTERLEAVE
-  static const unsigned char co_swap_function[4096] = {
-    0x48, 0x89, 0x26,        /* mov [rsi],rsp    */
-    0x48, 0x8b, 0x27,        /* mov rsp,[rdi]    */
-    0x58,                    /* pop rax          */
-    0x48, 0x89, 0x6e, 0x08,  /* mov [rsi+ 8],rbp */
-    0x48, 0x8b, 0x6f, 0x08,  /* mov rbp,[rdi+ 8] */
-    0x48, 0x89, 0x5e, 0x10,  /* mov [rsi+16],rbx */
-    0x48, 0x8b, 0x5f, 0x10,  /* mov rbx,[rdi+16] */
-    0x4c, 0x89, 0x66, 0x18,  /* mov [rsi+24],r12 */
-    0x4c, 0x8b, 0x67, 0x18,  /* mov r12,[rdi+24] */
-    0x4c, 0x89, 0x6e, 0x20,  /* mov [rsi+32],r13 */
-    0x4c, 0x8b, 0x6f, 0x20,  /* mov r13,[rdi+32] */
-    0x4c, 0x89, 0x76, 0x28,  /* mov [rsi+40],r14 */
-    0x4c, 0x8b, 0x77, 0x28,  /* mov r14,[rdi+40] */
-    0x4c, 0x89, 0x7e, 0x30,  /* mov [rsi+48],r15 */
-    0x4c, 0x8b, 0x7f, 0x30,  /* mov r15,[rdi+48] */
-    0xff, 0xe0,              /* jmp rax          */
-  };
-  #else
   static const unsigned char co_swap_function[4096] = {
     0x48, 0x89, 0x26,        /* mov [rsi],rsp    */
     0x48, 0x8b, 0x27,        /* mov rsp,[rdi]    */
@@ -117,10 +97,11 @@ static void (*co_swap)(cothread_t, cothread_t) = 0;
     0x4c, 0x8b, 0x7f, 0x30,  /* mov r15,[rdi+48] */
     0xff, 0xe0,              /* jmp rax          */
   };
-  #endif
 
-  #include <unistd.h>
-  #include <sys/mman.h>
+  #ifdef LIBCO_MPROTECT
+    #include <unistd.h>
+    #include <sys/mman.h>
+  #endif
 
   static void co_init() {
     #ifdef LIBCO_MPROTECT
@@ -141,24 +122,29 @@ cothread_t co_active() {
   return co_active_handle;
 }
 
-cothread_t co_create(unsigned int size, void (*entrypoint)(void)) {
+cothread_t co_derive(void* memory, unsigned int size, void (*entrypoint)(void)) {
   cothread_t handle;
   if(!co_swap) {
     co_init();
     co_swap = (void (*)(cothread_t, cothread_t))co_swap_function;
   }
   if(!co_active_handle) co_active_handle = &co_active_buffer;
-  size += 512;  /* allocate additional space for storage */
-  size &= ~15;  /* align stack to 16-byte boundary */
 
-  if(handle = (cothread_t)malloc(size)) {
-    long long *p = (long long*)((char*)handle + size);  /* seek to top of stack */
-    *--p = (long long)crash;                            /* crash if entrypoint returns */
-    *--p = (long long)entrypoint;                       /* start of function */
-    *(long long*)handle = (long long)p;                 /* stack pointer */
+  if(handle = (cothread_t)memory) {
+    unsigned int offset = (size & ~15) - 32;
+    long long *p = (long long*)((char*)handle + offset);  /* seek to top of stack */
+    *--p = (long long)crash;                              /* crash if entrypoint returns */
+    *--p = (long long)entrypoint;                         /* start of function */
+    *(long long*)handle = (long long)p;                   /* stack pointer */
   }
 
   return handle;
+}
+
+cothread_t co_create(unsigned int size, void (*entrypoint)(void)) {
+  void* memory = malloc(size);
+  if(!memory) return (cothread_t)0;
+  return co_derive(memory, size, entrypoint);
 }
 
 void co_delete(cothread_t handle) {
@@ -168,6 +154,10 @@ void co_delete(cothread_t handle) {
 void co_switch(cothread_t handle) {
   register cothread_t co_previous_handle = co_active_handle;
   co_swap(co_active_handle = handle, co_previous_handle);
+}
+
+int co_serializable() {
+  return 1;
 }
 
 #ifdef __cplusplus
