a2445692b3f63449c863ee559d0fc953dd67864d
Sintendo <bram.speeckaert@gmail.com>
2020-02-25 00:07:16 +0100

    Disable multi test

--- -test/cc.sh
+++ -test/cc.sh
@@ -4,5 +4,5 @@ gcc -O3 -fomit-frame-pointer -o libco.o -c ../libco.c
 g++ -O3 -fomit-frame-pointer -c test_timing.cpp
 g++ -O3 -fomit-frame-pointer -o test_timing libco.o test_timing.o
 
-g++ -O3 -fomit-frame-pointer -c test_timing_multi.cpp
-g++ -O3 -fomit-frame-pointer -o test_timing_multi test_timing_multi.o
+#g++ -O3 -fomit-frame-pointer -c test_timing_multi.cpp
+#g++ -O3 -fomit-frame-pointer -o test_timing_multi test_timing_multi.o
